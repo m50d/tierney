c397e1e4e3a703c1c6eee86a481cd92a2496f7d1
Michael Donaghy <md401@srcf.ucam.org>
2017-01-15 18:02:53 +0000

    Add FunctorK instance for FreeApplicative
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- core/src/main/scala/tierney/core/FunctorK.scala
+++ core/src/main/scala/tierney/core/FunctorK.scala
@@ -3,6 +3,7 @@ package tierney.core
 import cats.~>
 import cats.Functor
 import cats.free.Free
+import cats.free.FreeApplicative
 
 /**
  * Higher-kinded functor
@@ -15,6 +16,12 @@ object FunctorK {
      override def map[F[_], G[_]](f: F ~> G): Free[F, ?] ~> Free[G, ?] = new (Free[F, ?] ~> Free[G, ?]){
        override def apply[A](fa: Free[F, A]) =
          fa.foldMap[Free[G, ?]](f andThen[Free[G, ?]] Lambda[G ~> Free[G, ?]](Free.liftF(_)))
+     }
+  }
+  implicit def freeApplicativeFunctorK: FunctorK[FreeApplicative] = new FunctorK[FreeApplicative] {
+    override def map[F[_], G[_]](f: F ~> G): FreeApplicative[F, ?] ~> FreeApplicative[G, ?] = new (FreeApplicative[F, ?] ~> FreeApplicative[G, ?]){
+       override def apply[A](fa: FreeApplicative[F, A]) =
+         fa.foldMap[FreeApplicative[G, ?]](f andThen[FreeApplicative[G, ?]] Lambda[G ~> FreeApplicative[G, ?]](FreeApplicative.lift(_)))
      }
   }
 }
