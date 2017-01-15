ca1bb0a1c0fef63bdfc06f14ad96631c0e49c5b7
Michael Donaghy <md401@srcf.ucam.org>
2017-01-15 17:59:50 +0000

    Implement FunctorK for Free

--- core/src/main/scala/tierney/core/FunctorK.scala
+++ core/src/main/scala/tierney/core/FunctorK.scala
@@ -2,10 +2,19 @@ package tierney.core
 
 import cats.~>
 import cats.Functor
+import cats.free.Free
 
 /**
  * Higher-kinded functor
  */
 trait FunctorK[S[_[_], _]] {
   def map[F[_], G[_]](f: F ~> G): S[F, ?] ~> S[G, ?]
+}
+object FunctorK {
+  implicit def freeFunctorK: FunctorK[Free] = new FunctorK[Free] {
+     override def map[F[_], G[_]](f: F ~> G): Free[F, ?] ~> Free[G, ?] = new (Free[F, ?] ~> Free[G, ?]){
+       override def apply[A](fa: Free[F, A]) =
+         fa.foldMap[Free[G, ?]](f andThen[Free[G, ?]] Lambda[G ~> Free[G, ?]](Free.liftF(_)))
+     }
+  }
 }
--- free/src/main/scala/tierney/free/package.scala
+++ free/src/main/scala/tierney/free/package.scala
@@ -3,7 +3,22 @@ package tierney
 import cats.data.Coproduct
 import cats.free.FreeApplicative
 import cats.free.Free
+import tierney.core.FunctorKK
+import cats.~>
+import tierney.core.~~>
 
 package object free {
   type TierneyFreeF[S[_[_], _], F[_], A] = Coproduct[Free[S[F, ?], ?], FreeApplicative[Coproduct[F, S[F, ?], ?], ?], A]
+  object TierneyFreeF {
+//    implicit def functorKK: FunctorKK[TierneyFreeF] = new FunctorKK[TierneyFreeF] {
+//      override def map[S[_[_], _], T[_[_], _]](f: S ~~> T): Lambda[(F[_], A) => TierneyFreeF[S, F, A]] ~~> Lambda[(F[_], A) => TierneyFreeF[T, F, A]] =
+//        new (Lambda[(F[_], A) => TierneyFreeF[S, F, A]] ~~> Lambda[(F[_], A) => TierneyFreeF[T, F, A]]) {
+//        override def apply[F[_]]: TierneyFreeF[S, F, ?] ~> TierneyFreeF[T, F, ?] = new (TierneyFreeF[S, F, ?] ~> TierneyFreeF[T, F, ?])   {
+//          override def apply[A](fa: TierneyFreeF[S, F, A]) = fa.fold[TierneyFreeF[T, F, ?]](
+//            new (Free[S[F, ?], ? ~> )    
+//          )
+//        }
+//      }
+//    }
+  }
 }
