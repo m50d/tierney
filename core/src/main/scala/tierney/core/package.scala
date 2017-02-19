d645c5d69190822e405b2a1cabeac7d5b799f4f5
Michael Donaghy <md401@srcf.ucam.org>
2017-02-19 19:53:44 +0000

    Signature definitions

--- core/src/main/scala/tierney/core/package.scala
+++ core/src/main/scala/tierney/core/package.scala
@@ -13,14 +13,26 @@ package object core {
   }
 
   object FixK {
-    private[this] def unfixK[F[_[_], _]] = Lambda[FixK[F, ?] ~> F[FixK[F, ?], ?]](_.unfix)
-    def cataK[F[_[_], _], G[_]](f: F[G, ?] ~> G)(implicit functor: FunctorK[F]): FixK[F, ?] ~> G =
-      unfixK[F] andThen[F[G, ?]] functor.map[FixK[F, ?], G](new Lazy[FixK[F, ?], G](cataK(f))) andThen f
+    private[this] def unfixK[S[_[_], _]] = Lambda[FixK[S, ?] ~> S[FixK[S, ?], ?]](_.unfix)
+    def cataK[S[_[_], _], F[_]](f: S[F, ?] ~> F)(implicit functor: FunctorK[S]): FixK[S, ?] ~> F =
+      unfixK[S] andThen[S[F, ?]] functor.map[FixK[S, ?], F](new Lazy[FixK[S, ?], F](cataK(f))) andThen f
   }
-  
   final implicit class FixK[F[_[_], _], A](val unfix: F[FixK[F, ?], A]) {
     def cata[G[_]](f: F[G, ?] ~> G)(implicit functor: FunctorK[F]): G[A] = FixK.cataK(f).apply(this) 
   }
   
-  final implicit class FixKK[S[_[_[_], _], _[_], _], F[_], A](val unfix: S[Lambda[(F[_], A) => FixKK[S, F, A]], F, A])
+  final implicit class FixKK[W[_[_[_], _], _[_], _], F[_], A](val unfix: W[Lambda[(F[_], A) => FixKK[W, F, A]], F, A]) {
+//    def cata[S[_[_], _]](f: )
+  }
+}
+
+package core {
+object FixKK {
+    private[this] def unfixKK[W[_[_[_], _], _[_], _]] =
+      new (Lambda[(F[_], A) => FixKK[W, F, A]] ~~> Lambda[(F[_], A) => W[Lambda[(G[_], B) => FixKK[W, G, B]], F, A]]){
+        override def apply[F[_]] = Lambda[FixKK[W, F, ?] ~> W[Lambda[(F[_], A) => FixKK[W, F, A]], F, ?]](_.unfix)
+    }
+    def cataKK[W[_[_[_], _], _[_], _], S[_[_], _]](f:Lambda[(F[_], A) => W[S, F, A]] ~~> S)(implicit functor: FunctorKK[W]): Lambda[(F[_], A) => FixKK[W, F, A]] ~~> S =
+      null
+  }
 }
