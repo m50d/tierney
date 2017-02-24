15e2cbcf322d9b1569626adcfea79b4d0a805ff6
Michael Donaghy <md401@srcf.ucam.org>
2017-02-24 22:20:09 +0000

    Implement runParallelFans

--- free/src/main/scala/tierney/free/FreeSupport.scala
+++ free/src/main/scala/tierney/free/FreeSupport.scala
@@ -3,12 +3,14 @@ package tierney.free
 import cats.~>
 import cats.free.Free
 import tierney.core.FunctorK
+import cats.Monad
 
 trait FreeSupport {
   def compileF_[F[_], G[_]](f: F ~> G): Free[F, ?] ~> Free[G, ?] = Lambda[Free[F, ?] ~> Free[G, ?]](_.compile(f))
   implicit val compileF__ : FunctorK[Free] = new FunctorK[Free] {
     override def map[F[_], G[_]](f: F ~> G) = compileF_(f)
   }
+  def foldMapF_[F[_], G[_]: Monad](f: F ~> G): Free[F, ?] ~> G = Lambda[Free[F, ?] ~> G](_.foldMap(f))
   def liftF_[F[_]]: F ~> Free[F, ?] = Lambda[F ~> Free[F, ?]](Free.liftF(_))
 }
 object FreeSupport extends FreeSupport
--- free/src/main/scala/tierney/free/TierneyFree.scala
+++ free/src/main/scala/tierney/free/TierneyFree.scala
@@ -7,16 +7,18 @@ import CoproductSupport._
 import FreeApplicativeSupport._
 import FreeSupport._
 import cats.data.Coproduct
+import cats.~>
+import cats.arrow.FunctionK
 
 trait TierneyFree[F[_], A] extends Any {
   def parallel: Parallel[F, A]
   def serial: Serial[F, A]
   
-//  final def runParallelFans(implicit ap: Applicative[F]): Free[F, A] =
-//    serial.cata[Free](new (Lambda[(F[_], A) => SerialF[Free, F, A]] ~~> Free){
-//      override def apply[F[_]] =
-//        compileF_[ParallelFF[Free, F, ?], F](
-//          null
-//        )
-//    })(SerialF.functorKKSerialF)
+  final def runParallelFans(implicit ap: Applicative[F]): Free[F, A] =
+    serial.cata[Free](foldMapF_[ParallelFF[Free, F, ?], Free[F, ?]](
+        foldMap_[Coproduct[F, Free[F, ?], ?], Free[F, ?]](
+          foldCP_[F, Free[F, ?], Free[F, ?]](liftF_, FunctionK.id[Free[F, ?]])
+        )
+      )
+    )(SerialF.functorKKSerialF)
 }
