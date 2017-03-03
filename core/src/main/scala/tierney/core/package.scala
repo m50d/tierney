3d8399ca342663cdd547d036c94d352e1147962d
Michael Donaghy <md401@srcf.ucam.org>
2017-03-03 17:32:43 +0000

    Finish straightening out core

--- core/src/main/scala/tierney/core/package.scala
+++ core/src/main/scala/tierney/core/package.scala
@@ -4,9 +4,6 @@ import cats.Functor
 import cats.~>
 import cats.syntax.functor._
 
-/**
- * TODO: clear up which things are at top level vs companion object here
- */
 package object core {
   final implicit class Fix[F[_]](val unfix: F[Fix[F]]) {
     def cata[A](f: F[A] ⇒ A)(implicit functor: Functor[F]): A =
@@ -20,14 +17,15 @@ package object core {
     def cata[G[_]](f: F[G, ?] ~> G)(implicit functor: FunctorK[F]): G[A] = cataK(f).apply(this) 
   }
   
-  final implicit class FixKK[W[_[_[_], _], _[_], _], F[_], A](val unfix: W[Lambda[(G[_], B) => FixKK[W, G, B]], F, A]) {
-    def cata[S[_[_], _]](f: W[S, F, ?] ~> S[F, ?])(implicit functor: FunctorKK[W]): S[F, A] = FixKK.cataKK(f).apply(this)
-  }
   def fixKK[W[_[_[_], _], _[_], _], F[_]]: W[Lambda[(G[_], B) => FixKK[W, G, B]], F, ?] ~> FixKK[W, F, ?] =
     Lambda[W[Lambda[(G[_], B) => FixKK[W, G, B]], F, ?] ~> FixKK[W, F, ?]](FixKK(_))
-  object FixKK {
-    def unfixKK[W[_[_[_], _], _[_], _], F[_]] = Lambda[FixKK[W, F, ?] ~> W[Lambda[(G[_], B) => FixKK[W, G, B]], F, ?]](_.unfix)
-    def cataKK[W[_[_[_], _], _[_], _], S[_[_], _], F[_]](f: W[S, F, ?] ~> S[F, ?])(implicit functor: FunctorKK[W]): FixKK[W, F, ?] ~> S[F, ?] =
-      unfixKK[W, F] andThen[W[S, F, ?]] functor.map[Lambda[(G[_], A) => FixKK[W, G, A]], S, F](new LazyFunctionKK[Lambda[(G[_], A) => FixKK[W, G, A]], S, F](cataKK(f))) andThen[S[F, ?]] f
+  /** If the definition of this class is moved below the next two methods, we get a compile failure.
+   * This makes no sense, and I suspect a bug in kind-projector
+   */
+  final implicit class FixKK[W[_[_[_], _], _[_], _], F[_], A](val unfix: W[Lambda[(G[_], B) => FixKK[W, G, B]], F, A]) {
+    def cata[S[_[_], _]](f: W[S, F, ?] ~> S[F, ?])(implicit functor: FunctorKK[W]): S[F, A] = cataKK(f).apply(this)
   }
+  def unfixKK[W[_[_[_], _], _[_], _], F[_]] = Lambda[FixKK[W, F, ?] ~> W[Lambda[(G[_], B) => FixKK[W, G, B]], F, ?]](_.unfix)
+  def cataKK[W[_[_[_], _], _[_], _], S[_[_], _], F[_]](f: W[S, F, ?] ~> S[F, ?])(implicit functor: FunctorKK[W]): FixKK[W, F, ?] ~> S[F, ?] =
+    unfixKK[W, F] andThen[W[S, F, ?]] functor.map[Lambda[(G[_], A) => FixKK[W, G, A]], S, F](new LazyFunctionKK[Lambda[(G[_], A) => FixKK[W, G, A]], S, F](cataKK(f))) andThen[S[F, ?]] f
 }
