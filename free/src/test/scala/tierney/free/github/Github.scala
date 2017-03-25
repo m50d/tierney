40681066c5eb13496b44a7332c2651bd1ef64408
Michael Donaghy <md401@srcf.ucam.org>
2017-03-25 22:00:45 +0000

    Add examples and update for newer cats

---
+++ free/src/test/scala/tierney/free/github/Github.scala
@@ -0,0 +1,225 @@
+package de.codecentric.github
+
+import play.api.libs.json._
+import cats.Applicative
+import cats.`~>`
+import cats.data.Coproduct
+import cats.free.Free
+import cats.free.FreeApplicative
+import cats.instances.future._
+import cats.instances.list._
+import cats.instances.set._
+import cats.syntax.traverse._
+import play.api.libs.json._
+import scala.concurrent.Future
+
+sealed trait GitHub[A]
+case class GetComments(owner: Owner, repo: Repo, issue: Issue)
+    extends GitHub[List[Comment]]
+
+case class GetUser(login: UserLogin)
+    extends GitHub[User]
+
+case class ListIssues(owner: Owner, repo: Repo)
+    extends GitHub[List[Issue]]
+
+case class GetComment(owner: Owner, repo: Repo, id: Int)
+    extends GitHub[Comment]
+
+object GitHub {
+  private val ghApi = "https://api.github.com"
+
+  implicit val commentsEndpoint: Endpoint[GetComments] = {
+    new Endpoint[GetComments] {
+      def toUri(gc: GetComments) = gc match {
+        case GetComments(Owner(owner), Repo(repo), Issue(number)) =>
+          ghApi + s"/repos/$owner/$repo/issues/$number/comments"
+      }
+    }
+  }
+
+  implicit val userEndpoint: Endpoint[GetUser] = {
+    new Endpoint[GetUser] {
+      def toUri(gu: GetUser) = gu match {
+        case GetUser(UserLogin(login)) => ghApi + s"/users/$login"
+      }
+    }
+  }
+
+  implicit val listIssuesEndpoint: Endpoint[ListIssues] = {
+    new Endpoint[ListIssues] {
+      def toUri(li: ListIssues) = li match {
+        case ListIssues(Owner(owner),Repo(repo)) => ghApi + s"/repos/$owner/$repo/issues"
+      }
+    }
+  }
+
+  implicit val commentEndpoint: Endpoint[GetComment] = {
+    new Endpoint[GetComment] {
+      def toUri(gc: GetComment) = gc match {
+        case GetComment(Owner(owner),Repo(repo),id) => ghApi + s"/repos/$owner/$repo/issues/comments/$id"
+      }
+    }
+  }
+}
+
+object GitHubDsl extends Serializable {
+  type GitHubApplicative[A] = FreeApplicative[GitHub, A]
+  type GitHubMonadic[A] = Free[GitHub, A]
+  type GitHubBoth[A] = Free[Coproduct[GitHub,GitHubApplicative,?],A]
+
+  def getCommentsMonad(owner: Owner, repo: Repo, issue: Issue): GitHubMonadic[List[Comment]] =
+    Free.liftF(GetComments(owner, repo, issue))
+
+  def getCommentsM(owner: Owner, repo: Repo, issue: Issue): GitHubBoth[List[Comment]] =
+    Free.liftF[Coproduct[GitHub,GitHubApplicative,?],List[Comment]](
+      Coproduct.left[GitHubApplicative](GetComments(owner, repo, issue)))
+
+  def getComments(owner: Owner, repo: Repo, issue: Issue): GitHubApplicative[List[Comment]] =
+    FreeApplicative.lift(GetComments(owner, repo, issue))
+
+  def getUserMonad(login: UserLogin): GitHubMonadic[User] =
+    Free.liftF(GetUser(login))
+
+  def getUserM(login: UserLogin): GitHubBoth[User] =
+    Free.liftF[Coproduct[GitHub,GitHubApplicative,?],User](
+      Coproduct.left[GitHubApplicative](GetUser(login)))
+
+  def getUser(login: UserLogin): GitHubApplicative[User] =
+    FreeApplicative.lift(GetUser(login))
+
+  def listIssuesMonad(owner: Owner, repo: Repo): GitHubMonadic[List[Issue]] =
+    Free.liftF(ListIssues(owner,repo))
+
+  def listIssuesM(owner: Owner, repo: Repo): GitHubBoth[List[Issue]] =
+    Free.liftF[Coproduct[GitHub,GitHubApplicative,?],List[Issue]](
+      Coproduct.left[GitHubApplicative](ListIssues(owner,repo)))
+
+  def listIssues(owner: Owner, repo: Repo): GitHubApplicative[List[Issue]] =
+    FreeApplicative.lift(ListIssues(owner,repo))
+
+  def embed[A](p: GitHubApplicative[A]): GitHubBoth[A] =
+    Free.liftF[Coproduct[GitHub,GitHubApplicative,?],A](Coproduct.right(p))
+}
+
+object GitHubInterp {
+  import scala.concurrent.ExecutionContext.Implicits.global // don't do this
+  import GitHubDsl._
+
+  def step(client: Client): GitHub ~> Future =
+    new (GitHub ~> Future) {
+      def apply[A](fa: GitHub[A]): Future[A] = {
+        println(fa)
+        fa match {
+          case ffa@GetComments(_, _, _) => client.fetch(Endpoint(ffa)).map(parseComment)
+          case ffa@GetUser(_) => client.fetch(Endpoint(ffa)).map(parseUser)
+          case ffa@ListIssues(_,_) => client.fetch(Endpoint(ffa)).map(parseIssue)
+          case ffa@GetComment(_,_,_) => client.fetch(Endpoint(ffa)).map(parseSingleComment)
+        }
+      }
+    }
+
+  def stepAp(client: Client): GitHubApplicative ~> Future =
+    new (GitHubApplicative ~> Future) {
+      def apply[A](fa: GitHubApplicative[A]): Future[A] = fa.monad.foldMap(step(client))
+    }
+
+  def stepApPar(client: Client): GitHubApplicative ~> Future =
+    new (GitHubApplicative ~> Future) {
+      def apply[A](fa: GitHubApplicative[A]): Future[A] = fa.foldMap(step(client))
+    }
+
+  def stepApOpt(client: Client): GitHubApplicative ~> Future =
+    new (GitHubApplicative ~> Future) {
+      def apply[A](fa: GitHubApplicative[A]): Future[A] = {
+        val userLogins: List[UserLogin] =
+          fa.analyze(requestedLogins).toList
+
+        val fetched: Future[List[User]] =
+          userLogins.traverseU(u=>getUser(u)).foldMap(step(client))
+
+        val futureMapping: Future[Map[UserLogin,User]] =
+          fetched.map(userLogins.zip(_).toMap)
+
+        futureMapping.flatMap { mapping =>
+          fa.foldMap(prefetchedUsers(mapping)(step(client)))
+        }
+      }
+    }
+
+  def naturalLogging[F[_]]: F ~> F = new (F ~> F) {
+    def apply[A](fa: F[A]): F[A] = {
+      println(fa)
+      fa
+    }
+  }
+
+  val requestedLogins: GitHub ~> λ[α=>Set[UserLogin]] = {
+    new (GitHub ~> λ[α=>Set[UserLogin]]) {
+      def apply[A](fa: GitHub[A]): Set[UserLogin] = fa match {
+        case GetUser(u) => Set(u)
+        case GetComments(_,_,_) => Set.empty
+        case ListIssues(_,_) => Set.empty
+        case GetComment(_,_,_) => Set.empty
+      }
+    }
+  }
+
+  val requestedIssues: GitHub ~> λ[α=>Map[(Owner,Repo),Int]] = {
+    new (GitHub ~> λ[α=>Map[(Owner,Repo),Int]]) {
+      def apply[A](fa: GitHub[A]): Map[(Owner,Repo),Int] = fa match {
+        case GetComment(owner,repo,id) => Map((owner,repo) -> id)
+        case GetUser(_) => Map.empty
+        case ListIssues(_,_) => Map.empty
+        case GetComments(_,_,_) => Map.empty
+      }
+    }
+  }
+
+  def prefetchedUsers[F[_]:Applicative](prefetched: Map[UserLogin,User])(interp: GitHub ~> F): GitHub ~> F =
+    new (GitHub ~> F) {
+      def apply[A](fa: GitHub[A]): F[A] = fa match {
+        case GetComments(_,_,_) => interp(fa)
+        case ListIssues(_,_) => interp(fa)
+        case GetComment(_,_,_) => interp(fa)
+        case ffa@GetUser(login) =>
+          prefetched.get(login) match {
+            case Some(user) =>
+              Applicative[F].pure(user)
+            case None =>
+              interp(ffa)
+          }
+      }
+    }
+
+  private def parseComment(json: JsValue): List[Comment] = {
+    val objs = json.validate[List[JsValue]].get
+    objs.map { obj =>
+      (for {
+        url <- (obj \ "url").validate[String]
+        body <- (obj \ "body").validate[String]
+        login <- (obj \ "user" \ "login").validate[String]
+      } yield Comment(Url(url),Body(body),UserLogin(login))).get
+    }
+  }
+
+  private def parseSingleComment(obj: JsValue): Comment = {
+    (for {
+      url <- (obj \ "url").validate[String]
+      body <- (obj \ "body").validate[String]
+      login <- (obj \ "user" \ "login").validate[String]
+    } yield Comment(Url(url),Body(body),UserLogin(login))).get
+  }
+
+  private def parseUser(json: JsValue): User = {
+    (for {
+      login <- (json \ "login").validate[String]
+      name <- (json \ "name").validate[String] orElse (json \ "login").validate[String]
+    } yield User(login,name)).asOpt.get
+  }
+
+  private def parseIssue(json: JsValue): List[Issue] = {
+    val objs = json.validate[List[JsValue]].get
+    objs.map(obj => (obj \ "number").validate[Int].map(Issue(_)).asOpt).flatten
+  }
+}
---
+++ free/src/test/scala/tierney/free/github/Programs.scala
@@ -0,0 +1,255 @@
+package de.codecentric.github
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import cats._
+import cats.`~>`
+import cats.data.Coproduct
+import cats.instances.future._
+import cats.instances.list._
+import cats.instances.set._
+import cats.syntax.cartesian._
+import cats.syntax.traverse._
+import scala.concurrent.Future
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration._
+import scala.concurrent.{ Await, Future }
+
+trait ApplicativePrograms {
+  import GitHubDsl._
+  def getUsers(logins: List[UserLogin]
+  ): GitHubApplicative[List[User]] =
+    logins.traverseU(getUser)
+
+  def logins: GitHubApplicative[List[User]] =
+    List(UserLogin("markus1189"), UserLogin("..."), ???).
+      traverseU(login => getUser(login))
+
+  val issuesConcat: GitHubApplicative[List[Issue]] =
+    (listIssues(Owner("scala"),Repo("scala-dev"))
+      |@|
+      listIssues(Owner("scala"),Repo("slip"))
+    ).map(_++_)
+
+  val scalaIssues: GitHubApplicative[List[Issue]] =
+    List("scala","scala-dev","slip","scala-lang").
+      traverseU(repo =>
+        listIssues(Owner("scala"),Repo(repo))).
+      map(_.flatten)
+
+  def extractLogins(p: GitHubApplicative[_]): Set[UserLogin] = {
+    import GitHubInterp._
+    p.analyze(requestedLogins)
+  }
+
+  def precompute[A,F[_]:Applicative](
+    p: GitHubApplicative[A],
+    interp: GitHub ~> F
+  ): F[Map[UserLogin,User]] = {
+    val userLogins = extractLogins(p).toList
+
+    val fetched: F[List[User]] =
+      userLogins.traverseU(getUser).foldMap(interp)
+
+    Functor[F].map(fetched)(userLogins.zip(_).toMap)
+  }
+
+  def optimizeNat[F[_]:Applicative](
+    mapping: Map[UserLogin,User],
+    interp: GitHub ~> F
+  ): GitHub ~> F = new (GitHub ~> F) {
+    def apply[A](fa: GitHub[A]): F[A] = fa match {
+      case ffa@GetUser(login) =>
+        mapping.get(login) match {
+          case Some(user) => Applicative[F].pure(user)
+          case None => interp(ffa)
+        }
+      case _ => interp(fa)
+    }
+  }
+
+  def interpret: GitHub ~> Future = ???
+  def interpretOpt[A](p: GitHubApplicative[A])(implicit ec: scala.concurrent.ExecutionContext): Future[A] = {
+    val mapping: Future[Map[UserLogin,User]] = precompute(p,interpret)
+
+    mapping.flatMap { m =>
+      val betterNat = optimizeNat(m,interpret)
+      p.foldMap(betterNat)
+    }
+  }
+}
+
+trait Programs {
+  import GitHubDsl._
+
+  def branching = Monad[GitHubMonadic].ifM {
+    listIssuesMonad(Owner("foo"),Repo("bar")).map(_.nonEmpty)
+  }(listIssuesMonad(Owner("foo"),Repo("baz"))   //if true
+    ,listIssuesMonad(Owner("foo"),Repo("qux"))) //if false
+
+  def allUsers(
+    owner: Owner,
+    repo: Repo
+  ): GitHubMonadic[List[(Issue,List[(Comment,User)])]] = for {
+    issues <- listIssuesMonad(owner,repo)
+
+    issueComments <- issues.traverseU(issue =>
+      getCommentsMonad(owner,repo,issue).map((issue,_)))
+
+    users <- issueComments.traverseU { case (issue,comments) =>
+      comments.traverseU(comment =>
+        getUserMonad(comment.user).map((comment,_))).map((issue,_))
+    }
+  } yield users
+
+  def allUsersM(owner: Owner, repo: Repo):
+      GitHubBoth[List[(Issue,List[(Comment,User)])]] = for {
+
+    issues <- listIssuesM(owner,repo)
+
+        issueComments <- embed {
+          issues.traverseU(issue =>
+            getComments(owner,repo,issue).map((issue,_)))
+        }
+
+        users <- embed {
+          issueComments.traverseU { case (issue,comments) =>
+            comments.traverseU(comment =>
+              getUser(comment.user).map((comment,_))).map((issue,_))
+          }
+        }
+  } yield users
+
+  def userNamesFromIssueComments(
+    owner: Owner,
+    repo: Repo,
+    issue: Issue
+  ): GitHubBoth[List[User]] = for {
+    comments <- getCommentsM(owner, repo, issue)
+    users <- embed(comments.traverseU(comment => getUser(comment.user)) )
+  } yield users
+
+  def userNamesFromAllIssuesComments(
+    owner: Owner,
+    repo: Repo
+  ): GitHubBoth[List[List[User]]] = for {
+    issues <- listIssuesM(owner,repo)
+    issueComments <- embed(issues.traverseU(issue => getComments(owner,repo,issue)))
+    users <- embed { getUsers(issueComments) }
+  } yield users
+
+  def getUsers(issueComments: List[List[Comment]]): GitHubApplicative[List[List[User]]] =
+    issueComments.traverseU(comments =>
+      comments.traverseU(comment =>
+        getUser(comment.user)))
+}
+
+object Webclient {
+  implicit val ec = scala.concurrent.ExecutionContext.Implicits.global // don't do this
+  import GitHubDsl._
+
+  private def withClient[A](f: Client => A): A = {
+    implicit val sys: ActorSystem = ActorSystem(s"github-run-${util.Random.nextInt.abs}")
+    implicit val mat: ActorMaterializer = ActorMaterializer()
+    val client: Client = Client.ahcws
+
+    try {
+      f(client)
+    } finally {
+      client.close()
+      mat.shutdown()
+      sys.terminate()
+    }
+  }
+
+  val timeOut: FiniteDuration = 5.minutes
+
+  def applicative[A](p: GitHubApplicative[A]): A = {
+    import GitHubInterp._
+    withClient { client =>
+      Await.result(p.foldMap(step(client)), 5.minutes)
+    }
+  }
+
+  def monadic[A](p: GitHubMonadic[A]): A = {
+    import GitHubInterp._
+    withClient { client =>
+      Await.result(p.foldMap(step(client)), 5.minutes)
+    }
+  }
+
+  def both[A](p: GitHubBoth[A]): A = {
+    import GitHubInterp._
+    withClient { client =>
+      Await.result(p.foldMap(step(client).or[GitHubApplicative](stepApOpt(client))),
+        5.minutes)
+    }
+  }
+
+  def apply[A](
+    p: GitHubBoth[A],
+    dur: FiniteDuration = timeOut,
+    doApplicative: Boolean = false,
+    doMonadic: Boolean = false,
+    doOptimized: Boolean = false
+  ): Unit = {
+    withClient { client =>
+
+      if (doApplicative) {
+        val parallel: Coproduct[GitHub,GitHubApplicative,?] ~> Future = {
+          import GitHubInterp._
+          step(client).or[GitHubApplicative](stepApPar(client))
+        }
+
+        println("©"*80)
+        println("Applicative program (sleeping a moment):")
+        Thread.sleep(2000)
+        val resA = p.foldMap(parallel)
+
+        Await.result(resA,timeOut)
+      }
+
+      if (doMonadic) {
+        val sequential: Coproduct[GitHub,GitHubApplicative,?] ~> Future = {
+          import GitHubInterp._
+          step(client).or[GitHubApplicative](stepAp(client))
+        }
+        println("©"*80)
+        println("Monadic program:")
+        val resM = p.foldMap(sequential)
+
+        Await.result(resM,timeOut)
+      }
+
+      if (doOptimized) {
+        val optimized: Coproduct[GitHub,GitHubApplicative,?] ~> Future = {
+          import GitHubInterp._
+          step(client).or[GitHubApplicative](stepApOpt(client))
+
+        }
+
+        println("©"*80)
+        println("Applicative program OPTIMIZED (sleeping a moment):")
+        Thread.sleep(2000)
+        val resOpt = p.foldMap(optimized)
+
+        Await.result(resOpt,timeOut)
+      }
+    }
+  }
+}
+
+object MonadicDsl extends Programs {
+  def main(args: Array[String]): Unit =
+    println(Webclient.monadic(allUsers(Owner("scala"), Repo("scala"))))
+}
+
+object ApplicativeDsl extends ApplicativePrograms {
+  def main(args: Array[String]): Unit =
+    println(Webclient.applicative(scalaIssues))
+}
+
+object MixedDsl extends Programs {
+  def main(args: Array[String]): Unit =
+    println(Webclient.both(allUsersM(Owner("scala"),Repo("scala"))))
+}
